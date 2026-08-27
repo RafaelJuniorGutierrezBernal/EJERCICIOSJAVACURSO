f6f4ce00d5a004f63d92c50ff83ea3f02116622d
Rafael Junior <rafas8921@gmail.com>
2026-08-27 17:19:14 -0500

    Creacion inicial del proyecto SIPA y creacion de menu con variables de usuario

---
+++ 1/sipa.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class sipa {
+    public static void main() {
+        var salir = false;
+        var scanner = new Scanner(System.in);
+        var nombreUsuario = "";
+        var nombreFinca = "";
+
+        var menu = """
+                Bienvenido al SIPA
+                OPCIONES
+                1.Registrar Cosecha(Calculo de proyeccion)
+                2.Glosario de Terminos(Analisis de viabilidad)
+                3.Simulador de punto de equilibrio
+                4.Configuracion
+                5.Generar Reporte y salir""";
+
+        while (!salir) {
+
+        }
+
+    }
+}
