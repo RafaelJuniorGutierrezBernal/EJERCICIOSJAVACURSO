f7f5dad510252f706b0630a2f98ede2e8bb87fca
Rafael Junior <rafas8921@gmail.com>
2026-08-28 19:42:47 -0500

    feat: implement SIPA management system with DIAN tax calculations and unit conversion utilities

---
+++ 1/convertidorToneladas.java
@@ -0,0 +1,7 @@
+public class convertidorToneladas {
+
+    public static double convertidorToneladas(double toneladas) {
+        var kilosTotales = toneladas * 1000;
+        return kilosTotales;
+    }
+}
