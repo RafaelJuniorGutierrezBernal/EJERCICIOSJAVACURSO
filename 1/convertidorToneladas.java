8754f46c30898acafc50ee723fca818ede6d4433
Rafael Junior <rafas8921@gmail.com>
2026-08-31 22:23:53 -0500

    refactor: limpiar archivos obsoletos de raiz y estructurar paquetes modulares

--- 1/convertidorToneladas.java
+++
@@ -1,7 +0,0 @@
-public class convertidorToneladas {
-
-    public static double convertidorToneladas(double toneladas) {
-        var kilosTotales = toneladas * 1000;
-        return kilosTotales;
-    }
-}
