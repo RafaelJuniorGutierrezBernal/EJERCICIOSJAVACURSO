53747746a570c024c6635c4f2d5c6cce33ff9d54
Rafael Junior <rafas8921@gmail.com>
2026-09-01 21:28:20 -0500

    feat: add immutable customer record

--- 2/src/com/prestaya/model/Customer.java
+++ 2/src/com/prestaya/model/Customer.java
@@ -1,9 +1,9 @@
 package com.prestaya.model;
 
 public record Customer(
-        Long id,
-        String name,
-        String creditScore,
-        String city) {
+                String id,
+                String name,
+                int creditScore,
+                String city) {
 
 }
